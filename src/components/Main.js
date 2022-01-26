28d7ca5d9ffa278702a7492e3fa4e8c9420d7759
Jared Bradley <jaredbradley@jareds-mbp.lan>
2022-01-26 10:49:08 -0600

    update main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,39 @@
+import { useEffect, useState } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Index from '../pages/Index';
 import Show from '../pages/Show'
 
 const Main = (props) => {
+    const [ cheeses, setCheeses ] = useState(null);
+
+    const URL = "http://localhost:4000/cheese/";
+
+    const getCheeses = async () => {
+        const response = await fetch(URL);
+        const data = await response.json();
+        setCheeses(data);
+    };
+
+    const createCheeses = async (cheese) => {
+        //make post request to create cheeses
+        await fetch(URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "Application/JSON",
+            },
+            body: JSON.stringify(cheese),
+        });
+        //update list of cheese
+        getCheeses
+    };
+
+    useEffect(() => getCheeses(), []);
+
     return (
         <main>
             <Switch>
                 <Route exact path='/'>
-                    <Index />
+                    <Index cheeses = {cheeses} createCheese ={createCheeses}/>
                 </Route>
                 <Route path='/cheese/:id' render={(rc) => (<Show {...rc}/>)} />
             </Switch>
