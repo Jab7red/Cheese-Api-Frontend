3c806b04ba5c8d613a866b14894871c4c1c3503a
Jared Bradley <jaredbradley@jareds-mbp.lan>
2022-01-26 10:35:54 -0600

    import pages to main/add routes to main

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,18 @@
+import { Route, Switch } from 'react-router-dom';
+import Index from '../pages/Index';
+import Show from '../pages/Show'
+
 const Main = (props) => {
-    return <h1>Main</h1>
+    return (
+        <main>
+            <Switch>
+                <Route exact path='/'>
+                    <Index />
+                </Route>
+                <Route path='/cheese/:id' render={(rc) => (<Show {...rc}/>)} />
+            </Switch>
+        </main>
+    )
 };
 
 export default Main;
