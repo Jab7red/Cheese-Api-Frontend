1fdecb1d9417bc3a2537d9012a25b83fb0573d61
Jared Bradley <jaredbradley@jareds-mbp.lan>
2022-01-26 10:55:26 -0600

    update Index.js to show data

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,5 +1,23 @@
+import { Link } from "react-router-dom";
+
 const Index = (props) => {
-    return <h1>Index</h1>
+
+    //loaded function
+    const loaded = () => {
+        return props.cheeses.map((cheese) => (
+            <div key={cheese._id} className="cheese">
+                <Link to={`/cheeses/${cheese._id}`}><h1>{cheese.name}</h1></Link>
+                <img src={cheese.image} alt={cheese.name} />
+                <h3>{cheese.countryOfOrigin}</h3>
+            </div>
+        ));
+    };
+
+    const loading = () => {
+        return <h1>Loading...</h1>
+    };
+
+    return props.cheeses ? loaded() : loading();
 };
 
 export default Index;
